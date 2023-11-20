de3bede161d4421bc769aa80cd49b1f58f66f84b
Daniel Llopis <daniel.llopis@analog.com>
2023-11-20 21:57:54 +0000

    soc: Soc ADSP-SC83x Family support
    soc folder with adsp_sc83x family
    
    Signed-off-by: Daniel Llopis <daniel.llopis@analog.com>

---
+++ soc/adi/adspsc83x/soc.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023 Analog Devices, Inc
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC_SC83X_H_
+#define _SOC_SC83X_H_
+
+#ifndef _ASMLANGUAGE
+#include "system_cmsdk_adsp_sc83x.h"
+#include <zephyr/sys/util.h>
+#endif
+
+#endif /* _SOC_SC83X_H_ */
