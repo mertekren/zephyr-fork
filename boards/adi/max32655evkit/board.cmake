9d624ae23bf67ebcb29ff2bd264d03ebe2b8e15f
Maureen Helm <maureen.helm@analog.com>
2024-04-19 10:56:17 -0700

    boards: adi: max32655evkit: Add support for risc-v core
    Adds a new max32655evkit board target for the secondary risc-v core,
    demonstrating that we can boot zephyr and print "hello world" to a ram
    console. It has signficant limitations and isn't usable for real-world
    applications yet, but provides a common starting point for further
    development:
    
    - No kernel timer. The zero-riscy core doesn't implement a machine
      timer, so we need to enable a system-level timer peripheral to back
      the kernel timer.
    
    - No interrupts. The zero-riscy core only allows enabling/disabling
      interrupts globally and assumes there is an interrupt controller
      outside of the core. We need to implement support for that interrupt
      controller and rework the devicetree to point to it instead of the arm
      nvic.
    
    - No peripheral drivers. Without interrupts, the existing uart, gpio,
      and other peripheral drivers won't work. For now, we're just using a
      ram console.
    
    Signed-off-by: Maureen Helm <maureen.helm@analog.com>

--- boards/adi/max32655evkit/board.cmake
+++ boards/adi/max32655evkit/board.cmake
@@ -1,9 +1,14 @@
 # Copyright (c) 2023-2024 Analog Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(openocd --cmd-pre-init "source [find interface/cmsis-dap.cfg]")
-board_runner_args(openocd --cmd-pre-init "source [find target/max32655.cfg]")
-board_runner_args(jlink "--device=MAX32655" "--reset-after-load")
+if(CONFIG_BOARD_MAX32655EVKIT_MAX32655_M4)
+  board_runner_args(openocd --cmd-pre-init "source [find interface/cmsis-dap.cfg]")
+  board_runner_args(openocd --cmd-pre-init "source [find target/max32655.cfg]")
+  board_runner_args(jlink "--device=MAX32655" "--reset-after-load")
+elseif(CONFIG_BOARD_MAX32655EVKIT_MAX32655_RV32)
+  board_runner_args(openocd --cmd-pre-init "source [find interface/ftdi/olimex-arm-usb-ocd-h.cfg]")
+  board_runner_args(openocd --cmd-pre-init "source [find target/max32655_riscv.cfg]")
+endif()
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
