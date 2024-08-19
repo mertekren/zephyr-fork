9e554835e1d876a54d14bbb641b3dbe19917a22a
Mert Vatansever <mert.vatansever@analog.com>
2024-08-19 16:09:06 +0300

    drivers: flash: flash_max32: Enable additional FLC instances
    This commit enables additional FLC instances.
    
    Signed-off-by: Mert Vatansever <mert.vatansever@analog.com>

--- drivers/flash/flash_max32.c
+++ drivers/flash/flash_max32.c
@@ -1,17 +1,10 @@
 /*
- * Copyright (c) 2023 Analog Devices, Inc.
+ * Copyright (c) 2023-2024 Analog Devices, Inc.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define DT_DRV_COMPAT  adi_max32_flash_controller
-#define ADI_FLASH_NODE DT_INST(0, soc_nv_flash)
-
-#define FLASH_WRITE_BLK_SZ DT_PROP(ADI_FLASH_NODE, write_block_size)
-#define FLASH_ERASE_BLK_SZ DT_PROP(ADI_FLASH_NODE, erase_block_size)
-
-#define FLASH_BASE DT_REG_ADDR(ADI_FLASH_NODE)
-#define FLASH_SIZE DT_REG_SIZE(ADI_FLASH_NODE)
+#define DT_DRV_COMPAT adi_max32_flash_controller
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
@@ -19,6 +12,15 @@
 #include <zephyr/init.h>
 
 #include "flc.h"
+
+struct max32_flash_dev_config {
+	uint32_t flash_base;
+	uint32_t flash_erase_blk_sz;
+	struct flash_parameters parameters;
+#if CONFIG_FLASH_PAGE_LAYOUT
+	struct flash_pages_layout pages_layouts;
+#endif /* CONFIG_FLASH_PAGE_LAYOUT */
+};
 
 struct max32_flash_dev_data {
 #ifdef CONFIG_MULTITHREADING
@@ -49,32 +51,34 @@ static inline void max32_sem_give(const struct device *dev)
 
 static int api_read(const struct device *dev, off_t address, void *buffer, size_t length)
 {
-	ARG_UNUSED(dev);
+	const struct max32_flash_dev_config *const cfg = dev->config;
 
-	address += FLASH_BASE;
+	address += cfg->flash_base;
 	MXC_FLC_Read(address, buffer, length);
 	return 0;
 }
 
 static int api_write(const struct device *dev, off_t address, const void *buffer, size_t length)
 {
+	const struct max32_flash_dev_config *const cfg = dev->config;
 	int ret = 0;
 
 	max32_sem_take(dev);
 
-	address += FLASH_BASE;
+	address += cfg->flash_base;
 	ret = MXC_FLC_Write(address, length, (uint32_t *)buffer);
 
 	max32_sem_give(dev);
 
-	return ret;
+	return ret != 0 ? -EIO : 0;
 }
 
 static int api_erase(const struct device *dev, off_t start, size_t len)
 {
+	const struct max32_flash_dev_config *const cfg = dev->config;
+	uint32_t page_size = cfg->flash_erase_blk_sz;
+	uint32_t addr = (start + cfg->flash_base);
 	int ret = 0;
-	uint32_t page_size = FLASH_ERASE_BLK_SZ;
-	uint32_t addr = (start + FLASH_BASE);
 
 	max32_sem_take(dev);
 
@@ -94,40 +98,30 @@ static int api_erase(const struct device *dev, off_t start, size_t len)
 
 	max32_sem_give(dev);
 
-	return ret;
+	return ret != 0 ? -EIO : 0;
 }
 
 #if CONFIG_FLASH_PAGE_LAYOUT
-static const struct flash_pages_layout flash_max32_pages_layout = {
-	.pages_count = FLASH_SIZE / FLASH_ERASE_BLK_SZ,
-	.pages_size = FLASH_ERASE_BLK_SZ,
-};
-
 static void api_page_layout(const struct device *dev, const struct flash_pages_layout **layout,
 			    size_t *layout_size)
 {
-	*layout = &flash_max32_pages_layout;
+	const struct max32_flash_dev_config *const cfg = dev->config;
+
+	*layout = &cfg->pages_layouts;
 	*layout_size = 1;
 }
 #endif /* CONFIG_FLASH_PAGE_LAYOUT */
 
 static const struct flash_parameters *api_get_parameters(const struct device *dev)
 {
-	ARG_UNUSED(dev);
+	const struct max32_flash_dev_config *const cfg = dev->config;
 
-	static const struct flash_parameters parameters = {
-		.write_block_size = FLASH_WRITE_BLK_SZ,
-		.erase_value = 0xff,
-	};
-
-	return &parameters;
+	return &cfg->parameters;
 }
 
 static int flash_max32_init(const struct device *dev)
 {
-	int ret;
-
-	ret = MXC_FLC_Init();
+	int ret = MXC_FLC_Init();
 
 #ifdef CONFIG_MULTITHREADING
 	struct max32_flash_dev_data *data = dev->data;
@@ -135,7 +129,7 @@ static int flash_max32_init(const struct device *dev)
 	/* Mutex for flash controller */
 	k_sem_init(&data->sem, 1, 1);
 #endif
-	return ret;
+	return ret != 0 ? -EIO : 0;
 }
 
 static const struct flash_driver_api flash_max32_driver_api = {
@@ -148,7 +142,36 @@ static const struct flash_driver_api flash_max32_driver_api = {
 #endif
 };
 
-static struct max32_flash_dev_data flash_max32_data;
+#if CONFIG_FLASH_PAGE_LAYOUT
+#define FLASH_MAX32_CONFIG_PAGE_LAYOUT(n)                                                          \
+	.pages_layouts = {                                                                         \
+		.pages_count = DT_INST_FOREACH_CHILD(n, GET_FLASH_SIZE) /                          \
+			       DT_INST_FOREACH_CHILD(n, GET_ERASE_BLOCK_SIZE),                     \
+		.pages_size = DT_INST_FOREACH_CHILD(n, GET_ERASE_BLOCK_SIZE),                      \
+	},
+#else
+#define FLASH_MAX32_CONFIG_PAGE_LAYOUT(n)
+#endif
 
-DEVICE_DT_INST_DEFINE(0, flash_max32_init, NULL, &flash_max32_data, NULL, POST_KERNEL,
-		      CONFIG_FLASH_INIT_PRIORITY, &flash_max32_driver_api);
+#define GET_WRITE_BLOCK_SIZE(n) DT_PROP(n, write_block_size)
+#define GET_ERASE_BLOCK_SIZE(n) DT_PROP(n, erase_block_size)
+#define GET_FLASH_BASE(n)       DT_REG_ADDR(n)
+#define GET_FLASH_SIZE(n)       DT_REG_SIZE(n)
+
+#define DEFINE_FLASH_MAX32(_num)                                                                   \
+	static const struct max32_flash_dev_config max32_flash_dev_cfg_##_num = {                  \
+		.flash_base = DT_INST_FOREACH_CHILD(_num, GET_FLASH_BASE),                         \
+		.flash_erase_blk_sz = DT_INST_FOREACH_CHILD(_num, GET_ERASE_BLOCK_SIZE),           \
+		.parameters =                                                                      \
+			{                                                                          \
+				.write_block_size =                                                \
+					DT_INST_FOREACH_CHILD(_num, GET_WRITE_BLOCK_SIZE),         \
+				.erase_value = 0xFF,                                               \
+			},                                                                         \
+		FLASH_MAX32_CONFIG_PAGE_LAYOUT(_num)};                                             \
+	static struct max32_flash_dev_data max32_flash_dev_data_##_num;                            \
+	DEVICE_DT_INST_DEFINE(_num, flash_max32_init, NULL, &max32_flash_dev_data_##_num,          \
+			      &max32_flash_dev_cfg_##_num, POST_KERNEL,                            \
+			      CONFIG_FLASH_INIT_PRIORITY, &flash_max32_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(DEFINE_FLASH_MAX32)
